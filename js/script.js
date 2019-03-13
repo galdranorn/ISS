cd419ab8cfc95aab733b9070f3d8918f0813cfa7
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-03-13 20:25:52 +0100

    Add header, background and map styles

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
-let issSpeed = 'please wait';
-let issDistance = 0;
+let issSpeed = 'App is calculating...';
+let issDistance = 'please wait...';
+let issAvgSpeed = 7.66; // taken from wikipedia.com (en)
+let issInitialTimestamp = 911630999; // 1998-11-21 06:49:59 taken from isstracker.com
 let issPosition = { lat: 0, lng: 0, t: 0 };
 let issPastPosition = { lat: 0, lng: 0, t: 0 };
+
+document.getElementById('speed').innerHTML = issSpeed;
 
 // get initial state
 $.getJSON('http://api.open-notify.org/iss-now.json', function (data) {
@@ -10,10 +14,21 @@ $.getJSON('http://api.open-notify.org/iss-now.json', function (data) {
         lng: Number(data.iss_position.longitude),
         t: data.timestamp
     }
+    issDistance = (issPosition.t - issInitialTimestamp) * issAvgSpeed;
+    document.getElementById('distance').innerHTML = issDistance;
+    console.log('initial: '+issDistance);
 });
+
+// - - - - SPEED CALCULATION
 
 // update data with interval of 5s
 setInterval(function(){
+    document.getElementById('speed').innerHTML = issSpeed + ' km per second';
+    document.getElementById('distance').innerHTML = issDistance +' K kilometers';
+    
+    issDistance = (((issPosition.t - issInitialTimestamp) * issAvgSpeed)/1000).toFixed(2);
+    console.log('interval: '+issDistance);
+
     issPastPosition.lat = issPosition.lat;
     issPastPosition.lng = issPosition.lng;
     issPastPosition.t = issPosition.t;
@@ -59,22 +74,9 @@ const speedFromCoords = function (lat1, lng1, t1, lat2, lng2, t2) {
     let distance = R * c;
 
     let time = t2-t1; // [s]
-    issSpeed = distance/time;
+    issSpeed = (distance/time).toFixed(5);
 }
 
 let data = [
     { coords: { lat: 51.189, lng: 17.015 }}
 ];
-
-(function(){ 
-    	
-    window.initMap = function() { 
-      var initialLoc = data[0].coords;
-      
-      var map = new google.maps.Map(document.querySelector('#map'), {
-          zoom: 4,
-          center: initialLoc
-      });
-  }; 
-
-})();
